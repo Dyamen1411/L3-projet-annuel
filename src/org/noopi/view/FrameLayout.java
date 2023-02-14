967f92d0a8cdf4750bfbc53f930cb6fa7cc02109
Kheesi <fredric.tischler2@gmail.com>
2023-02-14 14:50:32 +0100

    Created the class FrameLayout implements IFrameLayout

---
+++ src/org/noopi/view/FrameLayout.java
@@ -0,0 +1,145 @@
+package org.noopi.view;
+
+import javax.swing.JComponent;
+import javax.swing.JMenuBar;
+
+import org.noopi.utils.listeners.AddRuleEventListener;
+import org.noopi.utils.listeners.NewFileEventListener;
+import org.noopi.utils.listeners.OpenFileEventListener;
+import org.noopi.utils.listeners.RemoveRuleEventListener;
+import org.noopi.utils.listeners.RunEventListener;
+import org.noopi.utils.listeners.SaveEventListener;
+import org.noopi.utils.listeners.SpeedChangeEventListener;
+import org.noopi.utils.listeners.StepEventListener;
+import org.noopi.utils.listeners.StopEventListener;
+import org.noopi.utils.listeners.TapeInitializationEventListener;
+
+public class FrameLayout implements IFrameLayout {
+
+    @Override
+    public JComponent getView() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public JMenuBar getMenuBar() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void shiftTapeRight() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void shiftTapeLeft() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setSymbolOnTape() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setMachineState() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addRule() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void removeRule() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resetRules() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pushHistory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void popHistory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addAddRuleEventListener(AddRuleEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addRemoveRuleEventListener(RemoveRuleEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addTapeInitializationEventListener(TapeInitializationEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addStepEventListener(StepEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addRunEventListener(RunEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addStopEventListener(StopEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addSpeedChangeEventListener(SpeedChangeEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addOpenFileEventListener(OpenFileEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addNewFileEventListener(NewFileEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addSaveEventListener(SaveEventListener l) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
