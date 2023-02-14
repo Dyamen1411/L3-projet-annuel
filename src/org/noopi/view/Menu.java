85ec1431648d18a91362eb780e76416e5c07f545
Kheesi <fredric.tischler2@gmail.com>
2023-02-14 14:47:35 +0100

    Created an enum class Menu for the MenuBar.

---
+++ src/org/noopi/view/Menu.java
@@ -0,0 +1,29 @@
+package org.noopi.view;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public enum Menu {
+    FILE("Fichier"),
+    EDIT("Aide");
+    
+    private String label;
+
+    Menu(String lb) {
+        label = lb;
+    }
+
+    String label() {
+        return label;
+    }
+    
+    static final Map<Menu, Item[]> STRUCT;
+    static {
+        STRUCT = new EnumMap<Menu, Item[]>(Menu.class);
+        STRUCT.put(Menu.FILE, new Item[] {
+                Item.NEW, Item.OPEN,
+                null,
+                Item.SAVE, Item.SAVE_AS
+            });
+    }
+}
