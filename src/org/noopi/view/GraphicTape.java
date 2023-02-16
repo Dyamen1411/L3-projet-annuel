6eaf4c1e16c9b554df8e8193973ca1e7784f58f9
Kheesi <fredric.tischler2@gmail.com>
2023-02-16 13:08:49 +0100

    Methods setState and setSymbol added in GraphicTape

--- src/org/noopi/view/GraphicTape.java
+++ src/org/noopi/view/GraphicTape.java
@@ -35,6 +35,14 @@ public class GraphicTape extends JComponent {
     }
 
     public void shiftTapeLeft() {
+
+    }
+
+    public void setState() {
+
+    }
+
+    public void setSymbol() {
         
     }
 
