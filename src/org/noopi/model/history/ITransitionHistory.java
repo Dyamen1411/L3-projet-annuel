a926c989c4f7d270b1617041f50385be4b33cd7b
Dyamen1411 <massias.antoine@gmail.com>
2023-02-16 10:32:21 +0100

    The Action data type was useless, a Transition was neater.

--- src/org/noopi/model/history/ITransitionHistory.java
+++ src/org/noopi/model/history/ITransitionHistory.java
@@ -4,15 +4,14 @@ import org.noopi.utils.listeners.history.HistoryPopEventListener;
 import org.noopi.utils.listeners.history.HistoryPushEventListener;
 import org.noopi.utils.listeners.history.HistoryResetEventListener;
 
-import org.noopi.utils.machine.Symbol;
 import org.noopi.utils.machine.Transition;
 
 public interface ITransitionHistory {
 
   void reset();
 
-  void pushAction(Action a);
-  Action popAction();
+  void pushAction(Transition a);
+  Transition popAction();
 
   boolean isEmpty();
 
@@ -23,22 +22,4 @@ public interface ITransitionHistory {
   void removeHistoryResetEventListener(HistoryResetEventListener l);
   void removeHistoryPushEventListener(HistoryPushEventListener l);
   void removeHistoryPopEventListener(HistoryPopEventListener l);
-
-  class Action {
-    public final Symbol symbol;
-    public final Transition transition;
-    
-    public Action(Symbol symbol, Transition transition) {
-      this.symbol = symbol;
-      this.transition = transition;
-    }
-
-    public Symbol getSymbol() {
-      return symbol;
-    }
-
-    public Transition getTransition() {
-      return transition;
-    }
-  }
 }
