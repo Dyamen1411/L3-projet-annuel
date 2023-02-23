4d6db05f0080a608142852b8fb8bdf5c8389e215
Dyamen1411 <massias.antoine@gmail.com>
2023-02-23 16:52:51 +0100

    Added & Implemented Tape->getSlice.

--- src/org/noopi/model/tape/TapeTester.java
+++ src/org/noopi/model/tape/TapeTester.java
@@ -5,8 +5,8 @@ import org.noopi.utils.machine.Symbol;
 
 public class TapeTester {
 
-  private static final Symbol DEFAULT_SYMBOL = new Symbol("_");
-  private static final Symbol TEST_SYMBOL = new Symbol("#");
+  private static final Symbol DEFAULT_SYMBOL = new Symbol("D");
+  private static final Symbol TEST_SYMBOL = new Symbol("T");
 
   public static void main(String[] args) {
     ITape tape = new Tape(DEFAULT_SYMBOL);
@@ -33,6 +33,12 @@ public class TapeTester {
 
     Symbol[] testList = { TEST_SYMBOL, DEFAULT_SYMBOL, DEFAULT_SYMBOL, TEST_SYMBOL };
     tape.reset(DEFAULT_SYMBOL, testList);
+    System.out.println("-----");
+    for (Symbol s : tape.getSlice(5)) {
+      System.out.print(s);
+    }
+    System.out.println("\n-----");
+
     for (Symbol s : testList) {
       assert tape.readSymbol().equals(s);
       tape.shift(Direction.RIGHT);
